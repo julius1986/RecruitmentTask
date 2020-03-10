7adcaba509093b527dd4c6d36236830481771718
Yuliy Yefymenko <uliy86@gmail.com>
2020-03-10 15:54:18 +0100

    add basic pagination

--- src/Components/TableOfCompanies/index.js
+++ src/Components/TableOfCompanies/index.js
@@ -8,17 +8,44 @@ import TableHeader from "../TableHeader";
 class TableOfCompanies extends Component {
   state = {
     companies:[],
+    companiesOnPage:20, 
+    currentPage:1, 
+    minPage:1, 
+    maxPage:1
   }
   async componentDidMount(){
     let companies = await getAllReadyCompanies();
-    this.setState({...this.state, companies});       
+    let maxPage = Math.round(companies.length/20);
+    this.setState({
+      ...this.state,
+      companies,
+      maxPage
+    });
+  }
+
+  nextPage = () => {
+    if(this.state.currentPage<this.state.maxPage){
+      this.setState( (state, props) => {
+       return { ...this.setState, currentPage: state.currentPage+1 }; 
+      })
+    }
+  }
+
+  prevPage = () => {
+    if (this.state.currentPage > this.state.minPage) {
+      this.setState((state, props) => {
+        return { ...this.setState, currentPage: state.currentPage - 1 };
+      });
+    }
   }
 
   render(){
     let rows = null;
-    let companies = this.state.companies; 
+    let from = this.state.currentPage * this.state.companiesOnPage - this.state.companiesOnPage;
+    let to = this.state.currentPage * this.state.companiesOnPage
+    let companies = this.state.companies.slice(from, to); 
     if(companies.length>0){                        
-      rows = this.state.companies.map(company => {                        
+      rows = companies.map(company => {                        
         return <TableRow key={company.id} company={company} />;                        
       });
     }
@@ -26,6 +53,7 @@ class TableOfCompanies extends Component {
       <div className="table-of-companies">
         <TableHeader/>
         {rows}
+        <button onClick={this.prevPage}>prev</button><button onClick={this.nextPage}>next</button>
       </div>
     );
   }
