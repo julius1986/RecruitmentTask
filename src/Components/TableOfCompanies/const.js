308a7be05731fc8521e1f979933165f8bfb74207
Yuliy Yefymenko <uliy86@gmail.com>
2020-03-13 09:56:46 +0100

    done with documentation

--- src/Components/TableOfCompanies/const.js
+++ src/Components/TableOfCompanies/const.js
@@ -1,17 +1,11 @@
-
-/**
- * @namespace TableOfCompanies
- */
 /**
  * Url for request list of companies
- * @memberof TableOfCompanies
  * @constant
  * @type {string}
  */
 const COMPANIES_URL = " https://recruitment.hal.skygate.io/companies";
 /**
  * Url for request income of companiy
- * @memberof TableOfCompanies
  * @constant
  * @type {string}
  */
